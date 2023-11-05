fa51ece54e5d09be0fe4743ae23f687a8cbcedc8
hasibullah1811 <hasib.ascend@gmail.com>
2023-11-06 01:22:19 +0600

    boundary-analysis and performance testing

--- unit_tests.py
+++ unit_tests.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 
 class MySeleniumUnitTest(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Chrome()
         self.base_url = "http://182.163.99.86"
         
 
@@ -34,6 +34,51 @@ class MySeleniumUnitTest(unittest.TestCase):
             self.assertEqual(h1.text, "Dashboard")
         except:
             self.fail("Login failed")
+
+    '''@author-Hasib'''
+    # No Locked state implemented
+    def test_max_incorrect_login_attempts(self):
+        """ Test the transition to a 'locked' state after maximum incorrect login attempts """
+        self.driver.get(self.base_url + "/login")
+        self.driver.implicitly_wait(10)
+        email_input = self.driver.find_element(By.XPATH, "//*[@id=\"username\"]")
+        password_input = self.driver.find_element(By.XPATH, "//*[@id=\"password\"]")
+
+        # Assuming the lockout mechanism is triggered after 10 incorrect attempts
+        for _ in range(10):
+            email_input.send_keys("admin@gigatech.com")
+            password_input.send_keys("Abc@1234")
+            login_button = self.driver.find_element(By.XPATH, "/html/body/div/div/div[2]/form/button")
+            login_button.click()
+            # Clear fields for the next attempt
+            email_input.clear()
+            password_input.clear()
+        
+        # Check for lockout message
+        lockout_message = self.driver.find_element(By.XPATH, "//div[contains(@class, 'lockout-message')]")
+        self.assertIn("Your account has been locked", lockout_message.text)
+
+    '''@author-Hasib'''
+    # Testing the Session Management of the Authentication feature
+    def test_session_management(self):
+        """ Test that the session is initiated upon login and terminated upon logout """
+        # Login first
+        self.test_login_successful()
+        # Check for a cookie that's set upon login, this is application specific
+        session_cookie = self.driver.get_cookie("session")
+        self.assertIsNotNone(session_cookie, "Session cookie should be set after login")
+
+        # Now logout
+        logout_button = self.driver.find_element(By.XPATH, "//a[@href='/logout']")
+        logout_button.click()
+
+        # Wait for logout to complete and redirect to login page
+        self.driver.implicitly_wait(10)
+        self.assertEqual(self.driver.current_url, self.base_url + "/login")
+
+        # Check that session cookie is cleared after logout
+        session_cookie = self.driver.get_cookie("session")
+        self.assertIsNone(session_cookie, "Session cookie should be cleared after logout")
 
    
 
